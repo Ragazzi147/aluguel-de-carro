7ddd1b25537b31b1618e316b6a131d8c82ae0b24
Ragazzi147 <lucasax147@gmail.com>
2023-08-30 14:35:02 -0300

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { footerLinks } from "@/constants";
+import { link } from "fs";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col
@@ -20,9 +23,38 @@ const Footer = () => {
           </p>
 
         </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title}
+            className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                  >
+                    {item.title}
+                  </Link>
+              ))}
+              </div>
+          )) }
+        </div>
+        </div>
 
-      </div>
-
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+          <div 
+          className="footer__copyrights-link">
+            <Link href="/">
+              Privacy Policy
+            </Link>
+            <Link href="/" 
+            className="text-gray-500">
+              Terms of Use
+            </Link>
+          </div>
+        </div>      
+ 
     </footer>
   )
 }
